67e0904dccc2fdc60624c8b2ac1b13b2db06a1d8
Anisha Kabir <ahsina.ribak@gmail.com>
2020-07-15 00:22:27 -0700

    added settings page to set default tip amount and update main ViewController

---
+++ tip/SettingsViewController.swift
@@ -0,0 +1,39 @@
+//
+//  SettingsViewController.swift
+//  tip
+//
+//  Created by Anisha Kabir on 7/14/20.
+//  Copyright © 2020 Codepath. All rights reserved.
+//
+
+import UIKit
+
+class SettingsViewController: UIViewController {
+
+    @IBOutlet weak var defaultTipControl: UISegmentedControl!
+    let defaults = UserDefaults.standard
+    var initialDefaultSaved = false
+        
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+    
+
+    @IBAction func defaultTipChange(_ sender: Any) {
+        defaults.set(defaultTipControl.selectedSegmentIndex, forKey: "defaultTip")
+        defaults.synchronize()
+        //print("added to defaults")
+        initialDefaultSaved = true
+    }
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
--- tip/ViewController.swift
+++ tip/ViewController.swift
@@ -10,11 +10,66 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var billField: UITextField!
+    @IBOutlet weak var tipLabel: UILabel!
+    @IBOutlet weak var totalLabel: UILabel!
+    @IBOutlet weak var tipControl: UISegmentedControl!
+    
+    let defaults = UserDefaults.standard
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        //print("view will appear")
+        // This is a good place to retrieve the default tip percentage from UserDefaults
+        // and use it to update the tip amount
+        
+        let bill =  Double(billField.text!) ?? 0
+        let tipPercentages = [0.15, 0.18, 0.2]
+        
+        let tip = bill*tipPercentages[defaults.integer(forKey: "defaultTip")]
+        let total = bill+tip;
+        
+        tipLabel.text = String(format: "$%.2f", tip)
+        totalLabel.text = String(format: "$%.2f", total)
+    }
 
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        //print("view did appear")
+    }
 
+    override func viewWillDisappear(_ animated: Bool) {
+        super.viewWillDisappear(animated)
+        //print("view will disappear")
+    }
+
+    override func viewDidDisappear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        //print("view did disappear")
+    }
+
+    @IBAction func onTap(_ sender: Any) {
+        view.endEditing(true)
+    }
+    
+    @IBAction func calculateTip(_ sender: Any) {
+        //get the bill amount
+        let bill =  Double(billField.text!) ?? 0
+        
+        //calculate tip and total
+        let tipPercentages = [0.15, 0.18, 0.2]
+        
+        let tip = bill*tipPercentages[tipControl.selectedSegmentIndex]
+        let total = bill+tip;
+        
+        //update tip and total
+        tipLabel.text = String(format: "$%.2f", tip)
+        totalLabel.text = String(format: "$%.2f", total)
+    }
 }
 
